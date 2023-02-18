c5147de7855eb86b4ef4f7ae8c9a17278db09ec3
Oleh Kurpiak <oleh.kurpiak@gmail.com>
2023-02-18 11:03:22 +0200

    init

---
+++ high-load-systems/hr-system/src/test/java/com/github/borsch/hrsystem/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.borsch.hrsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests extends EmbeddedNeo4jConfig {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
